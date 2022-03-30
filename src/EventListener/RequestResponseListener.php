0eb4754e27d2b7ff5f814565423ba7dba225a5d9
Théo Attali <theoattali@gmail.com>
2022-03-30 14:56:05 -0300

    fix sf bc 2

--- src/EventListener/RequestResponseListener.php
+++ src/EventListener/RequestResponseListener.php
@@ -93,7 +93,7 @@ class RequestResponseListener
     {
         // only handle master request, do not handle sub request like esi includes
         // If the device view is "not the mobile view" (e.g. we're not in the request context)
-        if ((HttpKernelInterface::MASTER_REQUEST ?? HttpKernelInterface::MAIN_REQUEST) !== $event->getRequestType() || $this->deviceView->isNotMobileView()) {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType() || $this->deviceView->isNotMobileView()) {
             return;
         }
 
